1c892dbf8353ebbb8ed5bdbd0731e68b6c6ec6e0
taehun33.kim <taehun33.kim@samsung.com>
2020-05-28 17:56:58 +0900

    Extend experiment dataset

--- train_script.sh
+++ train_script.sh
@@ -17,18 +17,31 @@ embedding_dim=$6
 
 echo ${model}, ${dataset}, ${loss}, ${epoch}, ${batch_size}, ${embedding_dim}
 
-retailrocket_path="--data_folder ../_data/retailrocket-prep --train_data retailrocket-train.csv --valid_data retailrocket-valid.csv --item2idx_dict item_idx_dict_filtered.pkl"
-yoochoose_path="--data_folder ../_data/yoochoose-prep/ --train_data yoochoose-train.csv --valid_data yoochoose-valid.csv"
+path_diginetica="--data_folder ../_data/diginetica-prep/ --train_data diginetica-train.csv --valid_data diginetica-valid.csv"
+path_reddit="--data_folder ../_data/reddit-prep/ --train_data reddit-train.csv --valid_data reddit-valid.csv"
+path_retailrocket="--data_folder ../_data/retailrocket-prep --train_data retailrocket-train.csv --valid_data retailrocket-valid.csv"
+path_sample="--data_folder ../_data/sample-prep/ --train_data sample-train.csv --valid_data sample-valid.csv"
+path_xing="--data_folder ../_data/xing-prep/ --train_data xing-train.csv --valid_data xing-valid.csv"
+path_yoochoose="--data_folder ../_data/yoochoose-prep/ --train_data yoochoose-train.csv --valid_data yoochoose-valid.csv"
+
 
 if [ $# == 0 ]; then
   echo "Please specify model and dataset!"
   exit 2
 fi
 
-if [ ${dataset} == "yoochoose" ]; then
-  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${yoochoose_path}
+if [ ${dataset} == "diginetica" ]; then
+  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${path_diginetica}
+elif [ ${dataset} == "reddit" ]; then
+  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${path_reddit}
 elif [ ${dataset} == "retailrocket" ]; then
-  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${retailrocket_path}
+  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${path_retailrocket}
+elif [ ${dataset} == "sample" ]; then
+  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${path_sample}
+elif [ ${dataset} == "xing" ]; then
+  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${path_xing}
+elif [ ${dataset} == "yoochoose" ]; then
+  python -u main.py --loss_type ${loss} --embedding_dim ${embedding_dim} --batch_size ${batch_size} --n_epochs ${epoch} ${path_yoochoose}
 
 else
     echo "(Error) There is no such model or dataset"
